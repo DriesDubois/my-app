ba79a7dd1d58e2f2fe026cfb7fc626be1516a190
Dries Dubois <dries.dubois@gmail.com>
2023-03-05 16:26:53 +0100

    Stuff n things

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",'./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',"./node_modules/flowbite/**/*.js"
+    "./src/**/*.{js,jsx,ts,tsx}",'./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js',"./node_modules/flowbite/**/*.js",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
